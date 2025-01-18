8f4fe9bc53d03a7526593d37a1cd3a33ab2e4c45
KermitWeb641 <kermitwebxd@gmail.com>
2025-01-17 22:17:19 -0500

    Update game.js
    Signed-off-by: KermitWeb641 <kermitwebxd@gmail.com>

--- game.js
+++ game.js
@@ -5,10 +5,13 @@ canvas.height = WINDOW_HEIGHT;
 const worldData = generateDetailedWorld();
 const camera = new Camera();
 const renderer = new Renderer(canvas, worldData, camera);
+const player = new Character(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
 
 function gameLoop() {
     camera.update();
+    player.update();
     renderer.draw();
+    player.draw(renderer.ctx, camera.x, camera.y);
     requestAnimationFrame(gameLoop);
 }
 
