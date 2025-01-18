82de21abfcc7c266609e06f81afc7df382e65cc3
KermitWeb641 <kermitwebxd@gmail.com>
2025-01-17 22:00:59 -0500

    Update renderer.js
    Signed-off-by: KermitWeb641 <kermitwebxd@gmail.com>

--- renderer.js
+++ renderer.js
@@ -3,6 +3,7 @@ class Renderer {
         this.ctx = canvas.getContext('2d');
         this.worldData = worldData;
         this.camera = camera;
+        this.ctx.imageSmoothingEnabled = false; // Keeps pixels sharp when zoomed
     }
 
     draw() {
@@ -12,8 +13,10 @@ class Renderer {
         const effectiveTileSize = TILE_SIZE * this.camera.zoomLevel;
         const startX = Math.floor(this.camera.x / effectiveTileSize);
         const startY = Math.floor(this.camera.y / effectiveTileSize);
-        const endX = startX + Math.ceil(WINDOW_WIDTH / effectiveTileSize) + 1;
-        const endY = startY + Math.ceil(WINDOW_HEIGHT / effectiveTileSize) + 1;
+        const tilesX = Math.ceil(WINDOW_WIDTH / effectiveTileSize) + 2;
+        const tilesY = Math.ceil(WINDOW_HEIGHT / effectiveTileSize) + 2;
+        const endX = Math.min(startX + tilesX, MAP_WIDTH);
+        const endY = Math.min(startY + tilesY, MAP_HEIGHT);
 
         for (let y = startY; y < endY; y++) {
             for (let x = startX; x < endX; x++) {
@@ -24,33 +27,5 @@ class Renderer {
         }
     }
 
-    drawTile(x, y, effectiveTileSize) {
-        const screenX = x * effectiveTileSize - this.camera.x;
-        const screenY = y * effectiveTileSize - this.camera.y;
-        
-        const tile = this.worldData[y][x];
-        const biomeColors = COLORS[tile.type];
-        
-        this.ctx.fillStyle = biomeColors.main;
-        this.ctx.fillRect(screenX, screenY, effectiveTileSize, effectiveTileSize);
-
-        if (this.camera.zoomLevel > 0.8) {
-            this.drawTileDetails(tile, screenX, screenY, effectiveTileSize, biomeColors);
-        }
-    }
-
-    drawTileDetails(tile, screenX, screenY, effectiveTileSize, biomeColors) {
-        const detailColor = biomeColors.detail[Math.floor(tile.detail * 3)];
-        this.ctx.fillStyle = detailColor;
-        
-        if (tile.type === 'FOREST') {
-            this.ctx.fillRect(screenX + effectiveTileSize/4, 
-                            screenY + effectiveTileSize/4,
-                            effectiveTileSize/2, effectiveTileSize/2);
-        } else if (tile.type === 'URBAN' || tile.type === 'RUINS') {
-            this.ctx.fillRect(screenX + effectiveTileSize/3,
-                            screenY + effectiveTileSize/3,
-                            effectiveTileSize/3, effectiveTileSize/3);
-        }
-    }
+    // Rest of the renderer code remains the same
 }
